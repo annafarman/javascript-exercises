79ff6fb13823e3ad07c5d921eb287b136c10bf0a
Anna Farman <anna.farman@gmail.com>
2024-01-31 23:49:05 -0500

    Complete JS exercise 05-07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celcius = (fahrenheit - 32) * (5/9);
+  if (celcius == 0) return 0;
+  else return parseFloat(celcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  const fahrenheit = (celcius * 9/5) + 32;
+  if (fahrenheit == 32) return 32;
+  else return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
