6ba75c41233f6e5e6e909a35a7df7454a1caf00a
Anna Farman <anna.farman@gmail.com>
2024-02-04 18:44:30 -0500

    Solve exercise 09-palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedStr = str.split('').reverse().join('');
+    console.log(str);
+    console.log(reversedStr);
+    return str == reversedStr;
+
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
