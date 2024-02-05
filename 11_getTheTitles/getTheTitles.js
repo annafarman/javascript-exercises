d6b91b3428689b2870cf109028d53885c1ba416a
Anna Farman <anna.farman@gmail.com>
2024-02-04 19:48:21 -0500

    Complete 11-getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(library) {
+    let bookTitle = library.map((book) => {
+         return book.title; 
+        });
+    return bookTitle;
 };
 
 // Do not edit below this line
